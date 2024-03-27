f8645dff8c7c6eac52bd89f6c05a0558307c3de8
Ярослав <yaroslaw74@gmail.com>
2024-03-27 17:17:01 +0200

    Create defines.php

---
+++ defines.php
@@ -0,0 +1,16 @@
+<?php
+/*********************************************************************************
+ * @Project    KYG Framework for business
+ * @Version    1.0.0
+ *
+ * @Copyright  (C) 2025 Kataev Yaroslav Georgievich 
+ * @E-mail     yaroslaw74@gmail.com
+ * @License    GNU General Public License version 3 or later; see LICENSE.md
+ ********************************************************************************/
+
+define('KYG_PATH_BASE', __DIR__ . DIRECTORY_SEPARATOR);
+define('KYG_PATH_CONFIG', KYG_PATH_BASE . 'config' . DIRECTORY_SEPARATOR);
+define('KYG_PAth_CONFIG_PACKAGES', KYG_PATH_CONFIG . 'packages' . DIRECTORY_SEPARATOR);
+define('KYG_PATH_MODULES', KYG_PATH_BASE . 'modules' . DIRECTORY_SEPARATOR);
+define('KYG_PATH_SRC', KYG_PATH_BASE . 'src' . DIRECTORY_SEPARATOR);
+define('KYG_PATH_PUBLIC', KYG_PATH_BASE . 'public' . DIRECTORY_SEPARATOR);
